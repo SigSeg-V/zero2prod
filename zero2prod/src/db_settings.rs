fd5f28ac348452b28cc27dc9982d52162529488c
Charlie <30825964+Vxlnce@users.noreply.github.com>
2023-05-20 11:59:47 +1000

    prod environment file compatibility

--- zero2prod/src/db_settings.rs
+++ zero2prod/src/db_settings.rs
@@ -1,8 +1,15 @@
+use std::path::PathBuf;
+
 use diesel::{PgConnection, Connection};
 use dotenvy;
 
 fn init_env() {
     dotenvy::dotenv().expect("Failed to init env var.");
+    match dotenvy::from_filename("prod.env").ok(){
+        None => println!("Warning: could not find prod.env"),
+        _ => ()
+    }
+
 }
 
 pub fn conn_str() -> String {
